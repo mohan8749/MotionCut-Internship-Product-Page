b3162a5fed8b3d070be6cc7f6f52298a6c78f76a
mohan8749 <143941106+mohan8749@users.noreply.github.com>
2024-01-11 22:57:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+    alert('Product added to cart!');
+    // Add logic to update cart or communicate with backend here
+});
